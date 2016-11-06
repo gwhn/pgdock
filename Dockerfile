24b0819846dab2160ec23125355df257f881aa9b
Guy Newman <guy@weblitz.co.uk>
2016-11-06 21:37:51 +0000

    feat: connect to postgres db

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 FROM golang
 ADD . /go/src/github.com/gwhn/pgdock
+RUN go get github.com/lib/pq
 RUN go install github.com/gwhn/pgdock
 ENTRYPOINT /go/bin/pgdock
 EXPOSE 8080
--- pgdock.go
+++ pgdock.go
@@ -4,13 +4,45 @@ import (
 	"net/http"
 	"log"
 	"fmt"
+	"database/sql"
+	_ "github.com/lib/pq"
 )
 
 func main() {
 	http.HandleFunc("/", defaultHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
-
+//postgres://postgres:1234@localhost/test_pg?sslmode=verify-full
 func defaultHandler(w http.ResponseWriter, _ *http.Request) {
-	fmt.Fprintln(w, "working")
+	fmt.Fprintln(w, "connecting to postgres")
+	db, err := sql.Open("postgres", "user=postgres password=1234 dbname=postgres host=test-pg sslmode=disable")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	defer db.Close()
+	fmt.Fprintln(w, "running query")
+	rows, err := db.Query("select id, recipe_name, category_name from recipes_with_category")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	defer rows.Close()
+	fmt.Fprintln(w, "scanning rows")
+	for rows.Next() {
+		var id int
+		var rname, cname string
+		err = rows.Scan(&id, &rname, &cname)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		fmt.Fprintf(w, "id=%d, recipe_name=%q, category_name=%q\n", id, rname, cname)
+	}
+	err = rows.Err()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	fmt.Fprintln(w, "completed successfully!")
 }
