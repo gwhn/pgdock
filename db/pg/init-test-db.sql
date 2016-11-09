88b8a10ff9fbb40e811157232543bf0fd857ca6b
Guy Newman <guy@weblitz.co.uk>
2016-11-09 12:22:45 +0000

    docs: comment about postgres encrypted password md5 format

--- db/pg/init-test-db.sql
+++ db/pg/init-test-db.sql
@@ -1,3 +1,4 @@
+-- postgres password format md5 -s "{user_name}{password}"
 CREATE USER test_user WITH ENCRYPTED PASSWORD 'md57bfb43fbd55dd6ef8af906df058882c2';
 CREATE DATABASE test_db;
 GRANT ALL PRIVILEGES ON DATABASE test_db TO test_user;
